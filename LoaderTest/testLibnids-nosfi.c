b39ba04348c5ada95f44444bdfeb920539345444
Yihan Dang <qcloud1014@gmail.com>
2021-03-28 17:01:20 +0800

    add binary to prevent recompiling libnids.so

--- LoaderTest/testLibnids-nosfi.c
+++ LoaderTest/testLibnids-nosfi.c
@@ -1,23 +1,17 @@
-/* Modified by Qcloud1223 
- * 1. change from dynamic linking to runtime linking
- * 2. suppress printf
-*/
-
 #include <libnet.h>
-#include <malloc.h>
 #include <nids.h>
 #include <pcap.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
 
-#include "../Loader/NanoNF.h"
-#include "../PrivateHeap.h"
-#include "../PrivateStack.h"
+//#pragma comment(lib, "ws2_32.lib")
+//#pragma comment(lib, "wpcap.lib")
+//#pragma comment(lib, "Packet.lib")
+//#pragma comment(lib, "libpcap.lib")
+//#pragma comment(lib, "libnids.lib")
+//#pragma comment(lib, "libnet.lib")
 
+//#pragma warning(disable : 4996)
 char ascii_string[10000];
 char *char_to_ascii(char ch) /* 用于把协议数据进行显示 */
 {
@@ -144,7 +138,7 @@ void tcp_protocol_callback(struct tcp_stream *tcp_connection, void **arg) {
                     //printf("客户端接收数据\n");
                     //for (i = 0; i < hlf->count_new; i++) {
                     //    printf("%s", char_to_ascii(content[i]));
-                    //    // 输出客户端接收的新的数据，以可打印字符进行显示
+                    // 输出客户端接收的新的数据，以可打印字符进行显示
                     //}
                     //printf("\n");
                 } else {
@@ -175,105 +169,30 @@ void tcp_protocol_callback(struct tcp_stream *tcp_connection, void **arg) {
     return;
 }
 
-void (*nids_run_local)();
-
-void nids_run_worker() {
-    struct timespec ts = {0, 0}, te = {0, 0};
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    nids_run_local();
-    /* Libnids进入循环捕获数据包状态 */
-    clock_gettime(CLOCK_MONOTONIC, &te);
-    // I really have no idea why this gives a SIGFPE...
-    // double t = ((double)te.tv_sec + 1.0e-9 * te.tv_nsec) -
-    //           ((double)ts.tv_sec + 1.0e-9 * ts.tv_nsec);
-    int sec_val = te.tv_sec - ts.tv_sec;
-    int nsec_val = sec_val * 1000000000;
-    int real_time = nsec_val + te.tv_nsec - ts.tv_nsec;
-    printf("Time elapsed: %d nanoseconds\n", real_time);
-
-    //we can't really switch back because RegSet of main stack is not stored
-    exit(0);
-}
-
-void start() {
-}
-
-int main(int argc, char *argv[], char **env) {
-    // printf("the address of argc is %p, argv is %p, env is %p\n", &argc, &argv, &env);
-    //the approach of early adjust the stack is probably bad...
-    int stackSize = sizeof(unsigned char) * (1 << 18);
-    //change stack size from 4<<10 to 1<<17, for there is a 65535 long array on stack...
-    void *s = malloc(stackSize);
-    printf("allocate stack at %p - %p\n", s, s + stackSize);
-
-    /* SetStack(0, s + stackSize - 0x10);
-    StackSwitch(0); */
-
+int main() {
     struct nids_chksum_ctl temp;
     temp.netaddr = 0;
     temp.mask = 0;
     temp.action = 1;
-#define HEAP_SIZE (1 << 24)
-    void *heap = malloc(sizeof(uint8_t) * HEAP_SIZE);
-    SetHeap(heap, HEAP_SIZE);
-    InitHeap();
-    printf("[mb] allocate heap start at: %p - %p\n", heap, heap + sizeof(uint8_t) * HEAP_SIZE);
-    //after this, all memory allocation functions are intercepted to Heap version
-
-    uint64_t len = NFusage_worker("/home/hypermoon/neptune-yh/binary/libnids.so.1.25", 0);
-    void *lib = memalign(getpagesize(), len);
-    ProxyRecord records[] = {{"malloc", HeapMalloc},
-                             {"free", HeapFree},
-                             {"realloc", HeapRealloc},
-                             {"calloc", HeapCalloc},
-                             NULL};
-    void *handle = NFopen("libnids.so", 0, lib, records, argc, argv, env);
-
-    //setting up heap bound, otherwise no nids function would actually run
-    uintptr_t *Heap_Lower_Bound = NFsym(handle, "_Gmem_Heap_Lower_Bound");
-    uintptr_t *Heap_Upper_Bound = NFsym(handle, "_Gmem_Heap_Upper_Bound");
-    uintptr_t *Stack_Lower_Bound = NFsym(handle, "_Gmem_Stack_Lower_Bound");
-    uintptr_t *Stack_Upper_Bound = NFsym(handle, "_Gmem_Stack_Upper_Bound");
-    uintptr_t *SO_Lower_Bound = NFsym(handle, "_Gmem_SO_Lower_Bound");
-    uintptr_t *SO_Upper_Bound = NFsym(handle, "_Gmem_SO_Upper_Bound");
-
-    *Heap_Lower_Bound = (unsigned long)heap;
-    *Heap_Upper_Bound = (unsigned long)(heap + sizeof(uint8_t) * HEAP_SIZE);
-    //we are tolerate here because I only care about nids_init()
-    *Stack_Lower_Bound = (unsigned long)(0x000000000000);
-    *Stack_Upper_Bound = (unsigned long)(0x7fffffffffff);
-    *SO_Lower_Bound = (unsigned long)lib;
-    *SO_Upper_Bound = (unsigned long)lib + len;
-
-    void (*register_chksum)(struct nids_chksum_ctl *, int) = NFsym(handle, "nids_register_chksum_ctl");
-    register_chksum(&temp, 1);
-
-    //nids_register_chksum_ctl(&temp, 1);
+    nids_register_chksum_ctl(&temp, 1);
     /*这段是相关与计算校验和的，比较新的网卡驱动会自动计算校验和，我们要做的就是把它关掉*/
-    struct nids_prm *nids_params = NFsym(handle, "nids_params");
-    nids_params->filename = "/home/toney/Pcap/traffic";
+    nids_params.filename = "/home/toney/Pcap/traffic";
     // nids_params.device = "all";
-    int (*nids_init)(void) = NFsym(handle, "nids_init");
-    char *nids_errbuf = NFsym(handle, "nids_errbuf");
-    //WARNING: THIS FUNCTION REFER TO MAIN STACK VARIABLES!!! FIXME
     if (!nids_init()) /* Libnids初始化 */
     {
         printf("Error：%s\n", nids_errbuf);
         exit(1);
     }
-    void (*nids_register_tcp)(void *) = NFsym(handle, "nids_register_tcp");
     nids_register_tcp((void *)tcp_protocol_callback);
-
-    nids_run_local = NFsym(handle, "nids_run");
     /* 注册回调函数 */
-
-    *Stack_Lower_Bound = (unsigned long)s;
-    *Stack_Upper_Bound = (unsigned long)(s + stackSize - 0x18);
-
-    SetStack(0, s + stackSize - 0x18);
-    StackStart(0, start);
-    SetStackPC(0, nids_run_worker);
-    StackSwitch(0);
-
+    struct timespec ts = {0, 0}, te = {0, 0};
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    nids_run();
+    /* Libnids进入循环捕获数据包状态 */
+    clock_gettime(CLOCK_MONOTONIC, &te);
+    int sec_val = te.tv_sec - ts.tv_sec;
+    unsigned long nsec_val = (unsigned long)sec_val * 1000000000UL;
+    unsigned long real_time = nsec_val + te.tv_nsec - ts.tv_nsec;
+    printf("Time elapsed: %lf seconds\n", (double)real_time / (double)1000000000);
     return 0;
 }
