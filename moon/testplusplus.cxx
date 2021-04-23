2ca3c08c425578da0ecd3b1615b851ce23e6c5ea
sgdxbc <sgdxbc@gmail.com>
2021-04-23 11:16:08 +0800

    Update Moon_Test++
    Signed-off-by: sgdxbc <sgdxbc@gmail.com>

--- moon/testplusplus.cxx
+++ moon/testplusplus.cxx
@@ -13,6 +13,9 @@ int main(int argc, char *argv[]) {
     printf("[test++] start\n");
     unsigned char *buffer = new unsigned char[128];
     printf("[test++] `new` return: %p\n", buffer);
+    vector<uint8_t> v;
+    v.reserve(1024);
+    printf("[test++] vector allocate at: %p\n", v.data());
     pcap_loop(nullptr, -1, onPacket, nullptr);
     return 0;
 }
