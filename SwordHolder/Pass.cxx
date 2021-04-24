a6f61d4149c103597b19a1f08dffbbfbdcf39806
sgdxbc <sgdxbc@gmail.com>
2021-04-24 10:48:58 +0800

    Switch to SwordHolder pass
    Signed-off-by: sgdxbc <sgdxbc@gmail.com>

--- SwordHolder/Pass.cxx
+++ SwordHolder/Pass.cxx
@@ -13,23 +13,22 @@ namespace
         static char ID;
         SwordHolderPass() : FunctionPass(ID) {}
 
-        virtual bool runOnFunction(Function &F)
+        virtual bool runOnFunction(Function &F) override
         {
-            outs() << "I saw a function called " << F.getName() << "!\n";
+            errs() << "I saw a function called " << F.getName() << "!\n";
             return false;
         }
     };
 }
 
 char SwordHolderPass::ID = 0;
+static RegisterPass<SwordHolderPass> X("SwordHolder", "Experimental SwordHolder Pass for HyperMOON", false, false);
 
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSwordHolderPass(const PassManagerBuilder &,
-                                    legacy::PassManagerBase &PM)
+static void loadPass(const PassManagerBuilder &Builder, legacy::PassManagerBase &PM)
 {
     PM.add(new SwordHolderPass());
 }
-static RegisterStandardPasses
-    RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                   registerSwordHolderPass);
+// These constructors add our pass to a list of global extensions.
+static RegisterStandardPasses Y_Ox(PassManagerBuilder::EP_ModuleOptimizerEarly, loadPass);
+// If the pass is enabled at any other points other than EP_EarlyAsPossible, we have to use EP_EnabledOnOptLevel0
+static RegisterStandardPasses Y_O0(PassManagerBuilder::EP_EnabledOnOptLevel0, loadPass);
