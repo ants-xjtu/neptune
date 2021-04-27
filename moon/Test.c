2e321aa9cbe5d3b9061dcc06061db415a1e5e019
sgdxbc <sgdxbc@gmail.com>
2021-04-27 20:10:51 +0800

    Update Test_Moon
    Signed-off-by: sgdxbc <sgdxbc@gmail.com>

--- moon/Test.c
+++ moon/Test.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int unknownValueSource[32];
+unsigned char g = '\0';
 
 int main(int argc, const char *argv[])
 {
-    unsigned char *p = malloc(sizeof(unsigned char) * 64);
-    printf("[Moon_Test] p = %p\n", p);
-    for (int i = 0; i < 16; i += 2)
-    {
-        p[i + 16] = unknownValueSource[i];
-    }
-    printf("p[42] = %d\n", p[42]);
+    unsigned char *h = malloc(sizeof(unsigned char) * 64);
+    unsigned char s;
+    printf("[Moon_Test] h = %p, &s = %p, &g = %p\n", h, &s, &g);
+
+    unsigned char *choices[3] = {&s, h, &g};
+
+    printf("Pick a base (0 = stack, 1 = heap, 2 = data): ");
+    int i;
+    scanf("%d", &i);
+    printf("Input an offset: ");
+    int off;
+    scanf("%d", &off);
+
+    printf("Attempt to write %p + (%d)...", choices[i], off);
+    choices[i][off] = 42;
+    printf(" done\n");
 
     return 0;
 }
